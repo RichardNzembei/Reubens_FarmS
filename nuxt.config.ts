7ec2c699c5e1ee28722138067942a0cb54c4c3dc
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-06 16:59:31 +0300

    update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    preset: "node", // Keeps this preset for server-side deployment
-  },
-
+ 
 
   modules: [
     
