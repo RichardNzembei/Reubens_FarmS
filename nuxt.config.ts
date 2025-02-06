f73bbfe6a1f63a6755d343841b6956d8a9398f43
RichardNzembei <richardsonreuben78@gmail.com>
2025-02-06 16:48:16 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    preset: "node", // Keeps this preset for server-side deployment
-  },
 
-  buildDir: '.output', // Ensure the output directory is correctly configured
-  app: {
-    buildAssetsDir: '.output/public', // Vercel will expect the public assets in here
-  },
 
   modules: [
+    
     '@nuxt/ui',
-    "@pinia/nuxt",
+    "@pinia/nuxt"
   ],
 })
